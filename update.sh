94140b67c8b371211ab3fb4367341577017f0b62
carlosbayarri <carlobr7@gmail.com>
2020-01-21 11:57:28 +0100

    dev branch

---
+++ update.sh
@@ -0,0 +1,18 @@
+
+echo "Updateing repo";
+read -p  "What's the commit? " commit;
+sudo git add *;
+echo "commit: $commit issue: $issue";
+if [ "$issue" != "" ]; then
+    if [ "$condition" != "" ]; then
+        sudo git commit -m "$commit" -m "$condition $issue";
+    else
+        sudo git commit -m "$commit" -m "$issue";
+    fi
+else
+        sudo git commit -m "$commit";
+fi
+
+sudo git push origin dev;
+
+echo "Updated!";
