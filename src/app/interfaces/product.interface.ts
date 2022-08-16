a48dbcdb4254fe7b7d48ed79b792253c98e0ae1d
CarlosBayarri <31616221+CarlosBayarri@users.noreply.github.com>
2022-08-16 15:18:38 +0000

    Deploying to main from  @ 1876674fb7ae8bbbf72425deae10330070474140 🚀

--- src/app/interfaces/product.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface Product {
-  type: string;
-  cod: string;
-  title: string;
-  url: string;
-}
--- src/app/shared/header/header.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from "@angular/core";
-import { HeaderComponent } from "./header.component";
-import { CommonModule } from '@angular/common';
-
-@NgModule({
-    imports: [CommonModule],
-    declarations: []
-  })
-  export class HeaderModule { }
